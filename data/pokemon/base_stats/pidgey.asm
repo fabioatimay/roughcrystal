52f6f3c3afea9a70c8a081f1c5dba369ad81bbbb
fabioatimay <163904375+fabioatimay@users.noreply.github.com>
2024-06-13 12:34:01 -0400

    pok2

--- data/pokemon/base_stats/pidgey.asm
+++ data/pokemon/base_stats/pidgey.asm
@@ -4,7 +4,7 @@
 	db NORMAL, FLYING ; type
 	db 255 ; catch rate
 	db 55 ; base exp
-	db NO_ITEM ; item 1
+	db FLOWER_MAIL ; item 1
 	db NO_ITEM ; item 2
 	dn GENDER_F50, 2 ; gender ratio, step cycles to hatch
 	INCBIN "gfx/pokemon/pidgey/front.dimensions"
